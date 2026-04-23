f1e2f74be14dc34b4b7654e50d5ebb9ce71b0935
Voyager1-u <uday.voyager1@gmail.com>
2026-04-23 11:55:32 +0530

    intial commit

---
+++ Py_Practise/code1.py
@@ -0,0 +1,44 @@
+#1 Reverse string
+'''
+data = input()
+# print(data[::-1])
+
+
+def reverse_string():
+    rev = ""
+    for i in data:
+            rev = i + rev
+    return rev
+
+print(reverse_string())
+'''
+
+
+#2 Palindrome
+
+'''data = input()
+# print(data == data[::-1])
+
+def palindrome_check():
+    rev = ""
+    for i in data:
+            rev = i + rev
+    return data.lower() == rev.lower()
+    # return data == rev
+
+
+print(palindrome_check())'''
+
+#Find duplicate elements in list
+
+'''l = [7,2,8,23,11,43,24,23,7,23,24]
+d=[]
+
+for i in l:
+    if l.count(i) > 1:
+        if i not in d:
+            d.append(i)
+print("duplicate elements are: ",d)'''
+
+
+
